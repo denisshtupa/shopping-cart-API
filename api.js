08e920f33a3802cf67b7efff8ad34d1e16483951
denis.shtupa <denis.shtupa@3i-solutions.net>
2021-01-17 22:14:54 +0100

    move products object to file product.js

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var cors = require('cors');
+var products = require('./product.js');
 var express = require('express');
 
 var app = express();
@@ -10,37 +11,7 @@ app.use('/api', router);
 
 router.route('/products-list').get((req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify([{
-            id: "f47f8d54-e93a-439c-9db0-28502d34dff3",
-            name: 'Car',
-            price: 15999,
-            quantity: 0
-        },
-        {
-            id: "431bbf0c-6bde-40ad-9a22-af6606074a92",
-            name: 'Jeans',
-            price: 59,
-            quantity: 0
-        },
-        {
-            id: "805a9d1c-eb6b-460c-bf6f-627b91e8329c",
-            name: 'Phone',
-            price: 899,
-            quantity: 0
-        },
-        {
-            id: "36d292b6-67b4-4425-b695-e59f6ff8b4ac",
-            name: 'Computer',
-            price: 1099,
-            quantity: 0
-        },
-        {
-            id: "977f8877-a480-4eb0-be55-19297f7001a9",
-            name: 'Book',
-            price: 19,
-            quantity: 0
-        }
-    ]));
+    res.end(JSON.stringify(products));
 });
 
 var port = process.env.PORT || 8090;
---
+++ product.js
@@ -0,0 +1,33 @@
+var products = [{
+        id: "f47f8d54-e93a-439c-9db0-28502d34dff3",
+        name: 'Car',
+        price: 15999,
+        quantity: 0
+    },
+    {
+        id: "431bbf0c-6bde-40ad-9a22-af6606074a92",
+        name: 'Jeans',
+        price: 59,
+        quantity: 0
+    },
+    {
+        id: "805a9d1c-eb6b-460c-bf6f-627b91e8329c",
+        name: 'Phone',
+        price: 899,
+        quantity: 0
+    },
+    {
+        id: "36d292b6-67b4-4425-b695-e59f6ff8b4ac",
+        name: 'Computer',
+        price: 1099,
+        quantity: 0
+    },
+    {
+        id: "977f8877-a480-4eb0-be55-19297f7001a9",
+        name: 'Book',
+        price: 19,
+        quantity: 0
+    }
+]
+
+module.exports = products;
